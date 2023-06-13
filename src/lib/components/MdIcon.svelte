8d1e3e410916d70c66984b34e43e9ce32da9b5a8
resultfulness <alicja.rrosiak@gmail.com>
2023-06-13 15:53:52 +0200

    allow for any icon size

--- src/lib/components/MdIcon.svelte
+++ src/lib/components/MdIcon.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   export let icon: string;
-  export let size: 18 | 24 | 36 | 48 = 24;
+  export let size: number = 24;
   export let variant: "outlined" | "round" | "sharp" | "two-tone" | "" = "";
   export let dark: boolean = false;
   export let color: string = "inherit";
