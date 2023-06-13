ae320111adde1efeffba937932569bda327a7b1d
resultfulness <alicja.rrosiak@gmail.com>
2023-06-13 21:09:25 +0200

    implement layout and styling of daily reminder card

--- src/routes/(navbar)/settings/+page.svelte
+++ src/routes/(navbar)/settings/+page.svelte
@@ -1,16 +1,29 @@
 <script>
   import Card from "$lib/components/Card.svelte";
+  import Reminder from "./Reminder.svelte";
   import ThemeSwitcher from "./ThemeSwitcher.svelte";
+
+  import "./settingsPageStyles.css";
 </script>
 
 <main>
   <Card>
     <ThemeSwitcher />
   </Card>
+  <hr />
+  <Card>
+    <Reminder />
+  </Card>
 </main>
 
 <style>
   main {
     margin: 2rem;
+  }
+
+  hr {
+    margin-block: 1.5rem;
+    border-color: var(--clr-separator);
+    filter: grayscale(0.5) opacity(0.6);
   }
 </style>
---
+++ src/routes/(navbar)/settings/Reminder.svelte
@@ -0,0 +1,76 @@
+<script>
+  import MdIcon from "$lib/components/MdIcon.svelte";
+  import Switch from "$lib/components/Switch.svelte";
+
+  let checked = false;
+
+  let showModal = false;
+</script>
+
+<div>
+  <div class="title">
+    <h2>Daily reminder</h2>
+    <Switch bind:checked />
+  </div>
+
+  <div class="buttons">
+    <button disabled={!checked}>
+      <MdIcon icon="notifications" />
+      <span>Launch notification</span>
+    </button>
+    <button on:click={() => (showModal = true)} disabled={!checked}>
+      <MdIcon icon="edit" />
+      <span>Hour: 20:00</span>
+    </button>
+  </div>
+
+  {#if !checked}
+    <p>Reminders are disabled!</p>
+  {/if}
+</div>
+
+<style>
+  .title {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  p {
+    text-align: center;
+    font-weight: 400;
+    font-size: 1.1rem;
+    color: var(--clr-alert);
+  }
+
+  .buttons {
+    display: flex;
+    justify-content: center;
+    gap: 0.75rem;
+    margin-block: 1rem;
+  }
+
+  button {
+    border-radius: 0.75rem;
+    border: 1px solid var(--clr-secondary-inactive);
+    background-color: var(--clr-bg);
+    font-weight: 600;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0.75rem;
+    padding-right: 1.25rem;
+    font-size: 1rem;
+    gap: 0.75rem;
+    color: var(--clr-text-inactive);
+    cursor: pointer;
+  }
+
+  button:disabled {
+    opacity: 0.5;
+    pointer-events: none;
+  }
+
+  button span {
+    color: var(--clr-text);
+  }
+</style>
