4ade30b76681a9f53520fd033ad828022139cade
resultfulness <alicja.rrosiak@gmail.com>
2023-06-13 14:46:01 +0200

    add material icons font and create icon component

---
+++ src/lib/components/MdIcon.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export let icon: string;
+  export let size: 18 | 24 | 36 | 48 = 24;
+  export let variant: "outlined" | "round" | "sharp" | "two-tone" | "" = "";
+  export let dark: boolean = false;
+  export let color: string = "";
+</script>
+
+<span
+  class:dark
+  class:light={!dark}
+  class="material-icons{variant && `-${variant}px`}"
+  style:font-size={size}
+  style:color
+>
+  {icon}
+</span>
+
+<style>
+  .dark {
+    color: rgba(0, 0, 0, 0.54);
+  }
+  .light {
+    color: rgba(255, 255, 255, 1);
+  }
+</style>
