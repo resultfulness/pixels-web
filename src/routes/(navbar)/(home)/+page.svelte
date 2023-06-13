13be003c9e87fea67c93a76b86b0f2c1e08caa1d
resultfulness <alicja.rrosiak@gmail.com>
2023-06-13 18:45:06 +0200

    implement the calendar view, including showing the entries

--- src/routes/(navbar)/(home)/+page.svelte
+++ src/routes/(navbar)/(home)/+page.svelte
@@ -1,1 +1,13 @@
-homepage
+<script lang="ts">
+  import Calendar from "./Calendar.svelte";
+</script>
+
+<main>
+  <Calendar />
+</main>
+
+<style>
+  main {
+    margin: 1rem 2rem 16rem;
+  }
+</style>
---
+++ src/routes/(navbar)/(home)/Calendar.svelte
@@ -0,0 +1,169 @@
+<script lang="ts">
+  import { pixelsData } from "$lib/stores";
+  import { onMount } from "svelte";
+
+  const MONTH_DAY_COUNTS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+  function getWeekdayOffset(year: number, month: number): number {
+    const weekday = new Date(`${year}-${month + 1}`).getDay();
+    if (weekday === 0) return 6;
+    else return weekday - 1;
+  }
+
+  $: dataSorted = $pixelsData.sort(
+    (a, b) => +new Date(a.date) - +new Date(b.date)
+  );
+
+  function getYearAndMonthFromDate(date: string): number[] {
+    const [year, month, _] = date.split("-");
+    return [+year, +month - 1];
+  }
+
+  $: [startYear, startMonth] = getYearAndMonthFromDate(dataSorted[0].date);
+  $: [endYear, endMonth] = getYearAndMonthFromDate(
+    dataSorted[dataSorted.length - 1].date
+  );
+
+  function getMapOfAllMonthsBetween(
+    startYear: number,
+    startMonth: number,
+    endYear: number,
+    endMonth: number
+  ): Map<number, number[]> {
+    const years = new Map();
+    let month = startMonth;
+    let year = startYear;
+    while (year <= endYear) {
+      years.set(year, []);
+      const limit = year === endYear ? endMonth : 11;
+      while (month <= limit) {
+        years.set(year, [...years.get(year), month]);
+        month++;
+      }
+      month = 0;
+      year++;
+    }
+
+    return years;
+  }
+
+  $: dates = getMapOfAllMonthsBetween(startYear, startMonth, endYear, endMonth);
+
+  onMount(() => {
+    for (const entry of dataSorted) {
+      const date = entry.date;
+
+      const year = document.querySelector(`#y${date.split("-")[0]}`)!;
+
+      const monthdays = year
+        .querySelector(`.m${+date.split("-")[1] - 1}`)!
+        .querySelectorAll<HTMLAnchorElement>(".day")!;
+
+      const dateMonthDay = +date.split("-")[2];
+      const dayOfEntry = monthdays[dateMonthDay - 1];
+
+      dayOfEntry.classList.add(`mood-level-${entry.scores[0]}`, `cup`);
+    }
+  });
+</script>
+
+{#each [...dates] as [year, months]}
+  <div class="year" id={`y${year}`}>
+    {#each months as month}
+      <div class={`m${month}`}>
+        <h2>
+          {new Date(1970, month).toLocaleString("default", {
+            month: "long",
+          })}
+        </h2>
+        <div class="monthdays">
+          {#each { length: getWeekdayOffset(year, month) } as _}
+            <div />
+          {/each}
+          {#each { length: MONTH_DAY_COUNTS[month] } as _, day}
+            <a href={`/entries/${year}/${month + 1}/${day + 1}`} class="day">
+              <span>{day + 1}</span>
+            </a>
+          {/each}
+        </div>
+      </div>
+    {/each}
+  </div>
+{/each}
+
+<style>
+  h2 {
+    text-align: center;
+    font-weight: 400;
+    font-size: 2rem;
+  }
+
+  .monthdays {
+    margin-block: 1rem 3rem;
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    gap: 0.25rem;
+  }
+
+  .day {
+    color: var(--clr-text);
+    aspect-ratio: 1;
+    background: repeating-linear-gradient(
+      -45deg,
+      var(--clr-dark-accent),
+      var(--clr-dark-accent) 8%,
+      transparent 8%,
+      transparent 16%
+    );
+    position: relative;
+  }
+
+  .day::after {
+    content: "";
+    position: absolute;
+    inset: 0;
+    background-color: black;
+    opacity: 0;
+    transition: opacity 200ms;
+  }
+
+  .day:hover::after {
+    opacity: 0.2;
+  }
+
+  .day span {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    display: grid;
+    place-items: center;
+    text-align: center;
+    background-color: var(--clr-bg);
+    border-radius: 50%;
+    aspect-ratio: 1;
+    width: 1.75em;
+    margin: 0.25em;
+    font-size: clamp(9px, 2.5vw, 15px);
+  }
+
+  .day.mood-level-1 {
+    background: none;
+    background-color: #7e57c2;
+  }
+  .day.mood-level-2 {
+    background: none;
+    background-color: #5c6bc0;
+  }
+  .day.mood-level-3 {
+    background: none;
+    background-color: #00bcd4;
+  }
+  .day.mood-level-4 {
+    background: none;
+    background-color: #9ccc65;
+  }
+  .day.mood-level-5 {
+    background: none;
+    background-color: #4caf50;
+  }
+</style>
