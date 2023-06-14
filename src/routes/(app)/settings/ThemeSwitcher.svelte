10dff4a74b417f7243709ae4c1e4d40f276ed610
resultfulness <alicja.rrosiak@gmail.com>
2023-06-14 18:32:32 +0200

    remove unneeded styles

--- src/routes/(app)/settings/ThemeSwitcher.svelte
+++ src/routes/(app)/settings/ThemeSwitcher.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { browser } from "$app/environment";
   import ButtonSplit from "$lib/components/ButtonSplit.svelte";
-  import MdIcon from "$lib/components/MdIcon.svelte";
 
   let darkMode: boolean | null = true;
 
@@ -57,38 +56,3 @@
     },
   ]}
 />
-
-<style>
-  div {
-    border-radius: 100vw;
-    width: max-content;
-    margin-inline: auto;
-    border: 1px solid var(--clr-secondary-inactive);
-    display: flex;
-    overflow: hidden;
-  }
-
-  button {
-    background-color: inherit;
-    color: var(--clr-text-inactive);
-    font-weight: 600;
-    padding: 1rem 1.5rem;
-    font-size: 1.1rem;
-    display: flex;
-    align-items: center;
-    gap: 0.5em;
-    border: none;
-    margin: 0;
-    cursor: pointer;
-  }
-
-  button:nth-of-type(2) {
-    border-left: 1px solid var(--clr-secondary-inactive);
-    border-right: 1px solid var(--clr-secondary-inactive);
-  }
-
-  .active {
-    background-color: var(--clr-secondary);
-    color: var(--clr-text-active);
-  }
-</style>
