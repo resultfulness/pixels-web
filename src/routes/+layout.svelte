f9aa15c001c7418c263a0de651807f9f5f43ca30
resultfulness <alicja.rrosiak@gmail.com>
2023-06-15 10:55:35 +0200

    remember scroll position on homepage after opening entry

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,26 @@
 <script lang="ts">
   import "../app.css";
+
+  import { afterNavigate, beforeNavigate } from "$app/navigation";
+  import { page } from "$app/stores";
+
+  let scrollY: number;
+
+  beforeNavigate(() => {
+    if ($page.url.pathname === "/")
+      sessionStorage.setItem("scrollpos", `${scrollY}`);
+  });
+
+  afterNavigate(() => {
+    let scrollpos = sessionStorage.getItem("scrollpos");
+
+    if (scrollpos && $page.url.pathname === "/") {
+      scrollY = +scrollpos;
+    }
+  });
 </script>
+
+<svelte:window bind:scrollY />
 
 <div id="app">
   <slot />
